98777ca4cc7c341d77a0efab26a892b836a64ae5
Tony <tonyselectronicslab@gmail.com>
2026-09-16 21:16:35 +0100

    Add files via upload

---
+++ atmega2560rgb.ino
@@ -0,0 +1,38 @@
+// Arduino Mega 2560 PWM Pins
+const int redPin   = 2; // PWM Pin 2
+const int greenPin = 3; // PWM Pin 3
+const int bluePin  = 4; // PWM Pin 4
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  // Smoothly fade from Red -> Green -> Blue -> Red
+  
+  // Red to Yellow to Green
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(redPin, 255 - i);
+    analogWrite(greenPin, i);
+    analogWrite(bluePin, 0);
+    delay(10);
+  }
+  
+  // Green to Cyan to Blue
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 255 - i);
+    analogWrite(bluePin, i);
+    delay(10);
+  }
+  
+  // Blue to Magenta to Red
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(redPin, i);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 255 - i);
+    delay(10);
+  }
+}
